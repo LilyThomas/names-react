6612f65369589dff7ed3609adbe51c973e964a4e
LilyThomas <anneliesthomas94@gmail.com>
2019-03-14 22:57:27 +0100

    jwt check added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { auth, strategies, requiredScopes } = require('express-oauth2-bearer');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
@@ -8,22 +11,48 @@ app.listen(process.env.PORT || 3000, () => {
 
 var names = ["Lily Thomas", "Simon Put"];
 
-var originsWhitelist = ['http://localhost:4200'];
-
 var corsOptions = {
     origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
+        callback(null, 'http://*');
     },
     credentials: true
 };
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://pettinder.eu.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'returnnames.com',
+    issuer: 'https://pettinder.eu.auth0.com/',
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    // console.log(req.headers);
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+//     next();
+// });
+//
+// app.use(auth(strategies.openid({
+//     issuerBaseURL: 'https://pettinder.eu.auth0.com/',
+//     allowedAudiences: 'https://returnnames.com'
+// })));
 
-    console.log(req.header('Authorization'));
-    res.json(names)
-})
+app.get('/',
+    // requiredScopes('apname.read'),
+    // (req, res) => {
+    // console.dir(req.auth.claims);
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    // res.sendStatus(200);
+    (req, res) => {
+    console.log(req.header("Autorization"))
+    res.json(names);
+});
 
