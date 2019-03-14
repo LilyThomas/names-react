3bfa0239ad0f3f67c8a66c44844abb70e5043839
LilyThomas <anneliesthomas94@gmail.com>
2019-03-15 00:49:04 +0100

    to do - scope and signature

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { auth, strategies, requiredScopes } = require('express-oauth2-bearer');
+const jwtAuthz = require('express-jwt-authz');
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 
@@ -20,6 +21,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+// const checkScopes = jwtAuthz([ 'read:apnames' ]);
+
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -37,7 +40,7 @@ var jwtCheck = jwt({
     issuer: 'https://pettinder.eu.auth0.com/',
     algorithms: ['RS256']
 });
-
+//
 app.use(jwtCheck);
 
 // app.use((req, res, next) => {
@@ -52,11 +55,8 @@ app.use(jwtCheck);
 // })));
 
 app.get('/',
-    // requiredScopes('apname.read'),
-    // (req, res) => {
-    // console.dir(req.auth.claims);
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    // res.sendStatus(200);
+    // checkScopes,
+
     (req, res) => {
     console.log(req.header('authorization'))
     res.json(names);
